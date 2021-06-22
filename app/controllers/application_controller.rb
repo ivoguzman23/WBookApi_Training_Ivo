2018cdb2c0608a46e6190bc6a9bcd3f59a88a220
ivoguzman23 <ivo.guzman@widergy.com>
2021-06-22 16:41:01 -0300

    remove .env file from cached.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   def render_resource(resource)    
-    #SE detiene el codigo en el return
     return render json: resource if resource.errors.empty?
+    
     validation_error(resource)
   end
 
