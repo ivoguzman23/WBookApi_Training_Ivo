c219dbfac41217d79241096a48dd8d93ee99bac9
ivoguzman23 <ivo.guzman@widergy.com>
2021-06-25 16:59:36 -0300

    .

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-    factory :book do
-      genre              { Faker::Book.genre }
-      author             { Faker::Book.author }
-      image              { Faker::Avatar.image }
-      title              { Faker::Book.title }
-      publisher          { Faker::Book.publisher  }
-      year               { Faker::Number.within(range: 1..10).to_s }
-    end
+  factory :book do
+    genre              { Faker::Book.genre }
+    author             { Faker::Book.author }
+    image              { Faker::Avatar.image }
+    title              { Faker::Book.title }
+    publisher          { Faker::Book.publisher }
+    year               { Faker::Number.within(range: 1..10).to_s }
   end
-  
+end
