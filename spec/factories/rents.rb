81a6cedbadad30810119bcaeb0656513ad236b00
ivoguzman23 <ivo.guzman@widergy.com>
2021-06-25 11:27:04 -0300

    Rent and User spec tests

--- spec/factories/rents.rb
+++ spec/factories/rents.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :rent do
-    from { "2021-06-23" }
-    to { "2021-06-23" }
+    from { Faker::Date.between(from: '2021-06-23', to: '2021-06-24') }
+    to { Faker::Date.between(from: '2021-06-25', to: '2021-06-26') }
+    user_id { '1' }
+    book_id { '1' }
   end
 end
---
+++ spec/models/rent_test_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+describe Rent, type: :model do
+  it do
+    should validate_presence_of(:from)
+  end
+
+  it do
+    should validate_presence_of(:to)
+  end
+
+  it do
+    should validate_presence_of(:user_id)
+  end
+
+  it do
+    should validate_presence_of(:book_id)
+  end
+
+  subject(:rent) do
+    build(:rent)
+  end
+
+  it 'is valid' do
+    is_expected.to be_valid
+  end
+
+  describe '#create' do
+
+    context 'When the from is nil' do
+      subject(:rent) do
+        build(:rent, from: nil)
+      end
+      it 'is not valid' do
+        is_expected.to be_invalid
+      end
+    end
+
+    context 'When the to is nil' do
+      subject(:rent) do
+        build(:rent, to: nil)
+      end
+      it 'is not valid' do
+        is_expected.to be_invalid
+      end
+    end
+
+    context 'When the user_id is nil' do
+      subject(:rent) do
+        build(:rent, user_id: nil)
+      end
+      it 'is not valid' do
+        is_expected.to be_invalid
+      end
+    end
+
+    context 'When the book_id is nil' do
+      subject(:rent) do
+        build(:rent, book_id: nil)
+      end
+      it 'is not valid' do
+        is_expected.to be_invalid
+      end
+    end
+
+  end
+
+end
