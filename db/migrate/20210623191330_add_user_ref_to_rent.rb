f6b6a0ed2867c93b9328481eee77b6bf5183fe55
ivoguzman23 <ivo.guzman@widergy.com>
2021-06-25 15:06:14 -0300

    Correct rubocop offenses

--- db/migrate/20210623191330_add_user_ref_to_rent.rb
+++ db/migrate/20210623191330_add_user_ref_to_rent.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddUserRefToRent < ActiveRecord::Migration[5.2]
   def change
     add_reference :rents, :user, foreign_key: true
--- spec/factories/rents.rb
+++ spec/factories/rents.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :rent do
     from { Faker::Date.between(from: '2021-06-23', to: '2021-06-24') }
