8b14a836b9741c9ef81a24bbe1beefc5541e3de7
ivoguzman23 <ivo.guzman@widergy.com>
2021-07-13 11:32:47 -0300

    .

--- db/schema.rb
+++ db/schema.rb
@@ -42,4 +42,7 @@ ActiveRecord::Schema.define(version: 20_210_622_133_710) do
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
+
+  add_foreign_key 'rents', 'books'
+  add_foreign_key 'rents', 'users'
 end
