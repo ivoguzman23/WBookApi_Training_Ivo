bbe8a3bed59c3f0b5e4b7d9f3871f8bade62e834
ivoguzman23 <ivo.guzman@widergy.com>
2021-07-13 10:43:17 -0300

    .

--- db/schema.rb
+++ db/schema.rb
@@ -37,22 +37,6 @@ ActiveRecord::Schema.define(version: 20_210_623_191_517) do
     t.index ['book_id'], name: 'index_rents_on_book_id'
     t.index ['user_id'], name: 'index_rents_on_user_id'
   end
-end
-
-ActiveRecord::Schema.define(version: 20_210_622_133_710) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
-
-  create_table 'books', force: :cascade do |t|
-    t.string 'genre'
-    t.string 'author'
-    t.string 'image'
-    t.string 'title'
-    t.string 'publisher'
-    t.string 'year'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-  end
 
   create_table 'users', force: :cascade do |t|
     t.string 'email', default: '', null: false
