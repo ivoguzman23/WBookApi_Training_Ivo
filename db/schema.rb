49371bd5588cfe30539894e0ffc83d9f0ec1c0ff
ivoguzman23 <ivo.guzman@widergy.com>
2021-07-13 11:35:20 -0300

    REsolve conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< routes_index_show_book
 ActiveRecord::Schema.define(version: 20_210_622_133_710) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
@@ -26,7 +25,7 @@ ActiveRecord::Schema.define(version: 20_210_622_133_710) do
     t.string 'year'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-=======
+
 ActiveRecord::Schema.define(version: 20_210_623_191_517) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
@@ -51,7 +50,6 @@ ActiveRecord::Schema.define(version: 20_210_623_191_517) do
     t.bigint 'book_id'
     t.index ['book_id'], name: 'index_rents_on_book_id'
     t.index ['user_id'], name: 'index_rents_on_user_id'
->>>>>>> master
   end
 
   create_table 'users', force: :cascade do |t|
