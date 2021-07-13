29470c3c2a127ade355f26e0aaf8289cae3ca0e8
ivoguzman23 <ivo.guzman@widergy.com>
2021-07-13 10:21:45 -0300

    Conflicts corrections

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-    validates :genre, :author, :image, :title, :publisher, :year, presence: true
-
+  validates :genre, :author, :image, :title, :publisher, :year, presence: true
+  has_one :rent
 end
