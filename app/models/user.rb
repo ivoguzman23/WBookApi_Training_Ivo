3394c3f66c5334e77e642f675a2b0c5bbd60a701
ivoguzman23 <ivo.guzman@widergy.com>
2021-07-14 09:21:15 -0300

    COnflicts corrections

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
          jwt_revocation_strategy: JWTBlacklist
 
   validates :first_name, :last_name, :email, :password, :password_confirmation, :locale, presence: true
+  has_many :rents
 end
