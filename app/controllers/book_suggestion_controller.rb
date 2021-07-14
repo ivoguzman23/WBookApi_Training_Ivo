52954b71119c4fb7a9328be7dfb6d0e8abf55a91
ivoguzman23 <ivo.guzman@widergy.com>
2021-07-14 16:04:07 -0300

    .

--- app/controllers/book_suggestion_controller.rb
+++ app/controllers/book_suggestion_controller.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
 class BookSuggestionController < ApplicationController
-    respond_to :json
+  respond_to :json
 
-    def create
-        @bookSuggestion = BookSuggestion.create(author: params[:bookSuggestion][:author], 
-                            title: params[:bookSuggestion][:title],
-                            link: params[:bookSuggestion][:link], publisher: params[:bookSuggestion][:publisher],
-                            year: params[:bookSuggestion][:year], user_id: current_user.id)
-        response.status = 201
-        render json: @bookSuggestion
-    end
+  def create
+    @bookSuggestion = BookSuggestion.create(author: params[:bookSuggestion][:author],
+                                            title: params[:bookSuggestion][:title],
+                                            link: params[:bookSuggestion][:link], 
+                                            publisher: params[:bookSuggestion][:publisher],
+                                            year: params[:bookSuggestion][:year], 
+                                            user_id: current_user.id)
+    response.status = 201
+    render json: @bookSuggestion
+  end
 end
