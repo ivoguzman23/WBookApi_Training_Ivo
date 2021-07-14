4ac2d5ae5b72fad3dcd1767f58747ba571b03b47
ivoguzman23 <ivo.guzman@widergy.com>
2021-07-14 14:42:07 -0300

    ADd the routes and book_suggestion_controller

--- app/controllers/book_suggestion_controller.rb
+++ app/controllers/book_suggestion_controller.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 class BookSuggestionController < ApplicationController
+    respond_to :json
+
+    def create
+        @bookSuggestion = BookSuggestion.create(author: params[:bookSuggestion][:author], 
+                            title: params[:bookSuggestion][:title],
+                            link: params[:bookSuggestion][:link], publisher: params[:bookSuggestion][:publisher],
+                            year: params[:bookSuggestion][:year], user_id: current_user)
+        response.status = 201
+        render json: @bookSuggestion
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20_210_623_191_517) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
