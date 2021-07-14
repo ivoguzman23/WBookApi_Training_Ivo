423db038cc91fd24488ccddd5d073b3f7973243b
ivoguzman23 <ivo.guzman@widergy.com>
2021-07-14 16:02:27 -0300

    .

--- app/controllers/book_suggestion_controller.rb
+++ app/controllers/book_suggestion_controller.rb
@@ -7,7 +7,7 @@ class BookSuggestionController < ApplicationController
         @bookSuggestion = BookSuggestion.create(author: params[:bookSuggestion][:author], 
                             title: params[:bookSuggestion][:title],
                             link: params[:bookSuggestion][:link], publisher: params[:bookSuggestion][:publisher],
-                            year: params[:bookSuggestion][:year], user_id: current_user)
+                            year: params[:bookSuggestion][:year], user_id: current_user.id)
         response.status = 201
         render json: @bookSuggestion
     end
