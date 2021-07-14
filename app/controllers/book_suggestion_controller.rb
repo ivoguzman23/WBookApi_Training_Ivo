f85511fed5c44379977dc7d6fc5354adbfa0bca9
ivoguzman23 <ivo.guzman@widergy.com>
2021-07-14 16:17:32 -0300

    Corrections

--- app/controllers/book_suggestion_controller.rb
+++ app/controllers/book_suggestion_controller.rb
@@ -4,13 +4,13 @@ class BookSuggestionController < ApplicationController
   respond_to :json
 
   def create
-    @bookSuggestion = BookSuggestion.create(author: params[:bookSuggestion][:author],
-                                            title: params[:bookSuggestion][:title],
-                                            link: params[:bookSuggestion][:link], 
-                                            publisher: params[:bookSuggestion][:publisher],
-                                            year: params[:bookSuggestion][:year], 
-                                            user_id: current_user.id)
+    @book_suggestion = BookSuggestion.create(author: params[:bookSuggestion][:author],
+                                             title: params[:bookSuggestion][:title],
+                                             link: params[:bookSuggestion][:link],
+                                             publisher: params[:bookSuggestion][:publisher],
+                                             year: params[:bookSuggestion][:year],
+                                             user_id: current_user.id)
     response.status = 201
-    render json: @bookSuggestion
+    render json: @book_suggestion
   end
 end
