5ae3d7c78352df73fdd284679d57f0fdb74f976e
ivoguzman23 <ivo.guzman@widergy.com>
2021-07-20 17:28:30 -0300

    Rubocop

--- app/controllers/book_suggestion_controller.rb
+++ app/controllers/book_suggestion_controller.rb
@@ -6,14 +6,16 @@ class BookSuggestionController < ApplicationController
 
   def index
     @booksuggestions = BookSuggestion.all
-    return render json: { error: 'There are no book suggestions to show' }, 
-                  status: :not_found if @booksuggestions.blank?
+    if @booksuggestions.blank?
+      return render json: { error: 'There are no book suggestions to show' },
+                    status: :not_found
+    end
 
     render_paginated @booksuggestions
   end
 
   def create
-    all_params =  book_suggestion_params.merge(user: current_user)
+    all_params = book_suggestion_params.merge(user: current_user)
     @book_suggestion = BookSuggestion.new(all_params)
     return render json: @book_suggestion, status: :created if @book_suggestion.save
 
