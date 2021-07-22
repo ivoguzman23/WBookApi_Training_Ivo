d6d8cd35ac584917ec9023929b05f4118d005254
ivoguzman23 <ivo.guzman@widergy.com>
2021-07-22 10:31:22 -0300

    Delete unnecesary comments

--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -3,9 +3,7 @@
 class OpenLibraryService
   def execute(bibkeys)
     apiurl = Rails.application.secrets.api.freeze
-    # params = {format: 'json', jscmd: 'data'}.to_query
     link = "#{apiurl}?bibkeys=#{bibkeys}&format=json&jscmd=data"
-    # puts "***********************" + link
     response = HTTParty.get(link)
     [response.code, response.parsed_response]
   end
